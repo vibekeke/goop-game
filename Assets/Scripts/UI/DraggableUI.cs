9f6feea124390a72e579eb2c7acf286cb67d3a51
rainbowshyyDev <99175473+rainbowshyyDev@users.noreply.github.com>
2025-07-18 15:25:09 +0200

    feat: add camera caching and cleanup math
    Cache camera to avoid Camera.main. Make draggable z position consistent on drop and drag.

--- Assets/Scripts/UI/DraggableUI.cs
+++ Assets/Scripts/UI/DraggableUI.cs
@@ -1,8 +1,7 @@
+using GoopGame.Utility;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using Image = UnityEngine.UI.Image;
-
+using UnityEngine.UI;
 
 namespace GoopGame.UI 
 {
@@ -11,9 +10,23 @@ namespace GoopGame.UI
     /// </summary>
     public class DraggableUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
-        public Image image;
-        [HideInInspector] public Transform parentAfterDrag; //holds the UI-slot the item should snap to
-        
+        [SerializeField]
+        private Image _image;
+
+        [HideInInspector]
+        public Transform ParentAfterDrag;   //holds the UI-slot the item should snap to
+
+        private Transform _cacheRoot;       //since we access the root several times, and don't expect this to change,
+                                            //we cache the value to avoid calling .GetRoot() several times.
+
+        private Transform _root {           //property field for root, to check if its cached.
+            get { 
+                if (_cacheRoot == null) 
+                    _cacheRoot = transform.root; 
+                return _cacheRoot; 
+            } 
+        }
+
         /// <summary>
         /// Movement when player clicks and drags a draggable UI element.
         /// When item is dragged, it is placed on top of the inventory hierarchy for visual purposes
@@ -21,21 +34,24 @@ namespace GoopGame.UI
         public void OnBeginDrag(PointerEventData eventData)
         {
             Debug.Log("Begin Drag");
-            parentAfterDrag = transform.parent;
-            transform.SetParent(transform.root); //Parents the item to the grid instead of the slots
-            transform.SetAsLastSibling(); //Sets it at the top of our view
-            image.raycastTarget = false;
+            ParentAfterDrag = transform.parent;
+            transform.SetParent(_root);             //Parents the item to the grid instead of the slots
+            transform.SetAsLastSibling();           //Sets it at the top of our view
+            _image.raycastTarget = false;
         }
 
 
         public void OnDrag(PointerEventData eventData)
         {
-            Debug.Log("Dragging");
-
             //Because Canvas Render mode is set to Camera, we need this extra math to calculate pos
             Vector3 mouseScreenPos = Input.mousePosition;
-            mouseScreenPos.z = Mathf.Abs(Camera.main.transform.position.z);
-            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+
+            //Access a cached camera field, since calling Camera.main each frame incurs overhead.
+            Vector3 mouseWorldPos = GlobalManager.Camera.WorldToScreenPoint(mouseScreenPos);
+            //Set the z position to be equal to the canvas (root object),
+            //since it already was a child of the canvas, the z position remains the same.
+            mouseWorldPos.z = _root.position.z;
+
             transform.position = mouseWorldPos;
         }
 
@@ -46,9 +62,9 @@ namespace GoopGame.UI
         public void OnEndDrag(PointerEventData eventData)
         {
             Debug.Log("End Drag");
-            transform.SetParent(parentAfterDrag);
+            transform.SetParent(ParentAfterDrag);
             transform.localPosition = Vector3.zero;
-            image.raycastTarget = true;
+            _image.raycastTarget = true;
         }
     }
 }
