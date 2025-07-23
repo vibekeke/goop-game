87dbf694bc8510ae0db8dc08e3efde0536b7e982
Lazo Omar <lazoo@ulrik.uio.no>
2025-07-23 02:15:30 +0200

    added temporary money count

--- Assets/Scripts/UI/ShopVisual.cs
+++ Assets/Scripts/UI/ShopVisual.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace GoopGame.UI
@@ -5,6 +6,7 @@ namespace GoopGame.UI
     public class ShopVisual : MonoBehaviour
     {
 
+        public int money = 500;
         public 
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,7 +18,7 @@ namespace GoopGame.UI
         // Update is called once per frame
         void Update()
         {
-        
+            
         }
     }
 }
