58e222158dc299bb2b664bd602de58e256228568
Vibeke Ihlen Selnes <107354551+vibekeke@users.noreply.github.com>
2025-07-24 01:50:24 +0200

    fix: moved dragging logic to InventoryItem
    Because I ended up with so many duplicate variables to similar things I moved the dragging logic to the InventoryItem script.

--- Assets/Scripts/UI/InventoryItem.cs
+++ Assets/Scripts/UI/InventoryItem.cs
@@ -1,19 +1,21 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using GoopGame.Utility;
 using GoopGame.Data;
 using GoopGame.Engine;
 using TMPro;
 
 namespace GoopGame.UI
 {
-    
+
     /// <summary>
     /// Script for the InventoryItem Prefab used in the UI.
     /// Uses ItemData scriptable object to display the correct images and text.
     /// </summary>
-    public class InventoryItem : MonoBehaviour
+    public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
-        [Header("Components")]
+        [Header("References")]
         [SerializeField]
         private Image _image;                  //Reference to the image renderer
 
@@ -28,14 +30,33 @@ namespace GoopGame.UI
         private string _displayDescription;
         private int _displayAmount = -1;
 
+        /// --- Drag and Drop ---
+        [HideInInspector]
+        public int ParentIndex;
+        public Transform ParentBeforeDrag;   //holds the UI-slot the item should snap to
 
-        public void Init(InventoryEntry entry)
+        private Transform _cacheRoot;       //since we access the root several times, and don't expect this to change,
+                                            //we cache the value to avoid calling .GetRoot() several times.
+
+        private Transform _root
+        {           //property field for root, to check if its cached.
+            get
+            {
+                if (_cacheRoot == null)
+                    _cacheRoot = transform.root;
+                return _cacheRoot;
+            }
+        }
+
+
+        public void Init(int slotIndex, InventoryEntry entry)
         {
             _itemData = entry.Item;
             _displayName = _itemData.Name;
             _displayDescription = _itemData.Description;
 
             _displayAmount = entry.Amount;
+            ParentIndex = slotIndex;
             UpdateUI();
         }
 
@@ -50,5 +71,51 @@ namespace GoopGame.UI
 
             //TODO: Hover over item to see its name and description
         }
+
+
+        // -- Drag and Drop Logic //
+        
+        /// <summary>
+        /// Movement when player clicks and drags a draggable UI element.
+        /// When item is dragged, it is placed on top of the inventory hierarchy for visual purposes.
+        /// </summary>
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            Debug.Log("Begin Drag");
+            ParentBeforeDrag = transform.parent;
+            transform.SetParent(_root);             //Parents the item to the grid instead of the slots
+            transform.SetAsLastSibling();           //Sets it at the top of our view
+            _image.raycastTarget = false;
+        }
+
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            //Because Canvas Render mode is set to Camera, we need this extra math to calculate pos
+            Vector3 mouseScreenPos = Input.mousePosition;
+
+            //Access a cached camera field, since calling Camera.main each frame incurs overhead.
+            Vector3 mouseWorldPos = GlobalManager.Camera.ScreenToWorldPoint(mouseScreenPos);
+            //Set the z position to be equal to the canvas (root object),
+            //since it already was a child of the canvas, the z position remains the same.
+            mouseWorldPos.z = _root.position.z;
+
+            transform.position = mouseWorldPos;
+        }
+
+        /// <summary>
+        /// By default, parentAfterDrag remains the same and so the item will snap back to it's original position.
+        /// However,  other scripts can change this variable to parent the UI-element to something else.
+        /// </summary>
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            Debug.Log("End Drag");
+            transform.SetParent(ParentBeforeDrag);
+            transform.localPosition = new Vector3(0, 0, 0);
+            _image.raycastTarget = true;
+        }
     }
 }
+
+
+
