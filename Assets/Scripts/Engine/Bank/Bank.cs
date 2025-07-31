c09402bc4bb3cf72693f9e85b6b642ced4b80c63
zonateduck <sjokokatten@gmail.com>
2025-07-31 17:58:22 +0200

    refactor: changed comments to be more consise and be capitalized

--- Assets/Scripts/Engine/Bank/Bank.cs
+++ Assets/Scripts/Engine/Bank/Bank.cs
@@ -3,49 +3,45 @@ using UnityEngine;
 namespace GoopGame.Engine
 {
     /// <summary>
-    /// bank class with current goop coins for game instance
-    /// to add and subtract from when completing quests and shopping
+    /// Bank class with current goop coins for game instance
+    /// To add and subtract from when completing quests and shopping
     /// </summary>
     public class Bank
     {
-        // goop coin variable for bank object, only to be changed within class
-        // with initial value 100
+        // Goop coin variable for bank object, only to be changed within class
+        // With initial value 100
         private const int GoopCoinsInitialValue = 100;
         public int GoopCoins { get; private set; }
 
         /// <summary>
-        /// constructor for new bank object
+        /// Constructor for new bank object
         /// <summary>
         public Bank()
         {
-            // set goop coin value to initial value on creation
+            // Set goop coin value to initial value on creation
             GoopCoins = GoopCoinsInitialValue;
         }
 
         /// <summary>
-        /// subtract method for bank instance,
-        /// removing set value from goop coin variable
-        /// returning success value 0 or 1
+        /// Method for subtracting set value from total goop coins
+        /// Returning success value true or false
         /// </summary>
         public bool Subtract(int subtractValue)
         {
-            // if subtract value is too much,
-            // coins cant be subtracted from total goop coins
-            // return success value false - failure
+            // Return false if it tried to subtract more goop coins than available
             if (subtractValue > GoopCoins)
             {
                 return false;
             }
 
-            // remove subtract value from total goop coins
-            // return success value true - success
+            // Subtract and update desired goop coins from total
+            // Returning true on success
             this.GoopCoins -= subtractValue;
             return true;
         }
 
         /// <summary>
-        /// add method for bank instance,
-        /// adding set value from goop coin variable
+        /// Method for adding set value to total goop coins
         /// </summary>
         public void Add(int addValue)
         {
--- Assets/Scripts/Utility/GlobalManager.cs
+++ Assets/Scripts/Utility/GlobalManager.cs
@@ -7,7 +7,7 @@ namespace GoopGame.Utility
     /// </summary>
     public class GlobalManager : MonoBehaviour
     {
-        // static, permanent global manager instance for game
+        // Static, permanent global manager instance for game
         public static GlobalManager Instance;
         
         /// <summary>
@@ -20,19 +20,20 @@ namespace GoopGame.Utility
         /// </summary>
         public static Camera Camera { get; private set; }
 
-        // uninitialized bank instance for game
+        // Uninitialized bank instance for game
         public Bank Bank;
 
         private void Awake()
         {
-            // check if Instance is set (not null), and that Instance isn't set to this instance
+            // Check if Instance is set (not null), and that Instance isn't set to this instance
             if (Instance != null && Instance != this)
             {
-            // if it already exists, destroy this GlobalManager instance, since it already exists
+            // If it already exists, destroy this GlobalManager instance, since it already exists
                 Destroy(gameObject);
                 return;
             }
-            // if we have not exited and GlobalManager is uninitialized, set Instance to this
+            // If we have not exited and GlobalManager is uninitialized, set Instance to this
+            // And set bank object to a new one
             Instance = this;
             Bank = new Bank();
 
